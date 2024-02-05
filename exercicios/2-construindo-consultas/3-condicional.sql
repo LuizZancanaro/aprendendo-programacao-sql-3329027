9b04193b9d3cfa0a448a95f6bc41deed05cff7cd
LuizZancanaro <86627362+LuizZancanaro@users.noreply.github.com>
2024-02-05 23:42:56 +0000

    resolucao

--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,2 +1,15 @@
 -- Escreva uma consulta que retorne o nome por extenso dos estados brasileiros
+Select DISTINCT
+Country as pais,
+State as estado_sigla,
+CASE
+  WHEN State = 'SP' THEN 'São Paulo'
+  WHEN State = 'RJ' THEN 'Rio de Janeiro'
+  ELSE 'Estado desconhecido'
+END AS estado
+FROM customers
+WHERE
+Country LIKE 'Brazil'
+;
+
 
